74ac4239fa75efb91b36f9d54b16630feaf7ef79
Arinji2 <arinjaydhar205@gmail.com>
2024-09-11 23:00:57 +0530

    Cleanup File Setup

--- completions/handler.go
+++ completions/handler.go
@@ -1,8 +1,6 @@
 package completions
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 )
@@ -20,38 +18,4 @@ func CompletionsHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Success"))
 
-}
-
-func GetBodyInput(r *http.Request) (string, error) {
-	var requestBody []interface{}
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-
-	if err := decoder.Decode(&requestBody); err != nil {
-		return "", errors.New("invalid input")
-
-	}
-
-	var prompt string
-	if len(requestBody) > 0 {
-
-		if bodyMap, ok := requestBody[0].(map[string]interface{}); ok {
-			if content, exists := bodyMap["content"].(string); exists {
-				prompt = content
-			} else {
-				return "", errors.New("invalid input format")
-			}
-		} else if bodyString, ok := requestBody[0].(string); ok {
-
-			prompt = bodyString
-		} else {
-			return "", errors.New("invalid input format")
-		}
-	} else {
-
-		return "", errors.New("empty input")
-	}
-
-	fmt.Println("Prompt:", prompt)
-	return prompt, nil
 }
---
+++ completions/input.go
@@ -0,0 +1,40 @@
+package completions
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+func GetBodyInput(r *http.Request) (string, error) {
+	var requestBody []interface{}
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(&requestBody); err != nil {
+		return "", errors.New("invalid input")
+
+	}
+
+	var prompt string
+	if len(requestBody) > 0 {
+
+		if bodyMap, ok := requestBody[0].(map[string]interface{}); ok {
+			if content, exists := bodyMap["content"].(string); exists {
+				prompt = content
+			} else {
+				return "", errors.New("invalid input format")
+			}
+		} else if bodyString, ok := requestBody[0].(string); ok {
+
+			prompt = bodyString
+		} else {
+			return "", errors.New("invalid input format")
+		}
+	} else {
+
+		return "", errors.New("empty input")
+	}
+
+	return prompt, nil
+}
