1d005ad1bb6d413ea540f7a105fd1c813067e100
Arinji2 <arinjaydhar205@gmail.com>
2024-09-21 19:59:56 +0530

    Add unit test for AddingRequests to task manager

--- tasks/task-manager_test.go
+++ tasks/task-manager_test.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -26,5 +27,37 @@ func TestNewTaskManager(t *testing.T) {
 	}
 
 	testingTaskManager = taskManager
+
+}
+
+func TestAddRequest(t *testing.T) {
+	TestNewTaskManager(t)
+	testingTaskManager.AddRequest("test", true)
+
+	if len(testingTaskManager.AllTasks.Tasks["test"].QueuedProcesses) != 1 {
+		t.Error("Task not added correctly")
+	}
+
+	testingTaskManager.RemoveRequest("test", testingTaskManager.AllTasks.Tasks["test"])
+	totalRequests := 10
+
+	for i := 0; i < totalRequests; i++ {
+		testingTaskManager.AddRequest(fmt.Sprintf("test%d", i), true)
+	}
+
+	firstQueuedProcesses := len(testingTaskManager.AllTasks.Tasks["test"].QueuedProcesses)
+	secondQueuedProcesses := len(testingTaskManager.AllTasks.Tasks["test2"].QueuedProcesses)
+
+	if (firstQueuedProcesses + secondQueuedProcesses) != totalRequests {
+		t.Error("Task not added correctly")
+	}
+
+	if firstQueuedProcesses == totalRequests || secondQueuedProcesses == totalRequests {
+		t.Error("Tasks not distributed equally", firstQueuedProcesses, secondQueuedProcesses)
+	}
+
+	for i := 0; i < totalRequests; i++ {
+		testingTaskManager.RemoveRequest(fmt.Sprintf("test%d", i), testingTaskManager.AllTasks.Tasks["test"])
+	}
 
 }
