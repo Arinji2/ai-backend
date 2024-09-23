883aa3e8b6275731f9bd5a839daa154f28db91f7
Arinji2 <arinjaydhar205@gmail.com>
2024-09-23 13:02:56 +0530

    Refactor TestUpdateOverloaded

--- tasks/task_test.go
+++ tasks/task_test.go
@@ -11,118 +11,12 @@ func TestUpdateOverloaded(t *testing.T) {
 	TestNewTaskManager(t)
 
 	singleQueueOverload(t)
-	taskManagerInstance.AllTasks.Tasks["test1"].TaskMu.Lock()
-	for i := 0; i < 4; i++ {
-		taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses = append(taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses, &QueuedProcess{
-			Prompt:      fmt.Sprintf("test%d in test1", i),
-			Done:        make(chan ResponseChan),
-			TimeStarted: time.Now(),
-		})
-	}
-	taskManagerInstance.AllTasks.Tasks["test1"].TaskMu.Unlock()
-	taskManagerInstance.AllTasks.Tasks["test2"].TaskMu.Lock()
-	for i := 0; i < 4; i++ {
-		taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses = append(taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses, &QueuedProcess{
-			Prompt:      fmt.Sprintf("test%d in test2", i),
-			Done:        make(chan ResponseChan),
-			TimeStarted: time.Now(),
-		})
-	}
-	taskManagerInstance.AllTasks.Tasks["test2"].TaskMu.Unlock()
+	allQueueOverload(t)
 
-	readyChanOne := make(chan bool)
-	readyChanTwo := make(chan bool)
-
-	pendingChanOne := make(chan bool)
-	pendingChanTwo := make(chan bool)
-
-	queueOne := taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses[0]
-	taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses = taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses[1:]
-	taskManagerInstance.AllTasks.Tasks["test1"].UpdateOverloaded(queueOne, readyChanOne, pendingChanOne)
-	queueTwo := taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses[0]
-	taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses = taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses[1:]
-	taskManagerInstance.AllTasks.Tasks["test2"].UpdateOverloaded(queueTwo, readyChanTwo, pendingChanTwo)
-	taskManagerInstance.PendingTasks.PendingMu.RLock()
-	if len(taskManagerInstance.PendingTasks.PendingQueue) != 8 {
-		t.Error("Pending tasks not adding up to 8", len(taskManagerInstance.PendingTasks.PendingQueue))
-	}
-	taskManagerInstance.PendingTasks.PendingMu.RUnlock()
-
-	var wg sync.WaitGroup
-	wg.Add(4) // We expect 4 signals
-
-	go func() {
-		defer wg.Done()
-		select {
-		case isReady := <-readyChanOne:
-			if !isReady {
-				t.Error("Task one did not become ready after overload")
-			}
-		case <-time.After(10 * time.Second):
-			t.Error("Timeout waiting for task one to become ready")
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		select {
-		case isReady := <-readyChanTwo:
-			if !isReady {
-				t.Error("Task two did not become ready after overload")
-			}
-		case <-time.After(10 * time.Second):
-			t.Error("Timeout waiting for task two to become ready")
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		select {
-		case isReady := <-pendingChanOne:
-			if !isReady {
-				t.Error("Pending one did not become ready after overload")
-			}
-
-		case <-time.After(10 * time.Second):
-			t.Error("Timeout waiting for pending one to become ready")
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		select {
-		case isReady := <-pendingChanTwo:
-			if !isReady {
-				t.Error("Pending two did not become ready after overload")
-			}
-		case <-time.After(10 * time.Second):
-			t.Error("Timeout waiting for pending two to become ready")
-		}
-	}()
-
-	// Use a timeout for the entire wait
-	if waitTimeout(&wg, 15*time.Second) {
-		t.Error("Timeout waiting for all channels to receive signals")
-	}
-
-	taskManagerInstance.AllTasks.Tasks["test1"].TaskMu.RLock()
-	taskManagerInstance.AllTasks.Tasks["test2"].TaskMu.RLock()
-	if len(taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses)+len(taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses) != 8 {
-		t.Error("Tasks not adding up to 8", len(taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses), len(taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses))
-	}
-
-	if len(taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses) == 0 {
-		t.Error("Test1 queue empty after overload", len(taskManagerInstance.AllTasks.Tasks["test1"].QueuedProcesses))
-	}
-
-	if len(taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses) == 0 {
-		t.Error("Test2 queue empty after overload", len(taskManagerInstance.AllTasks.Tasks["test2"].QueuedProcesses))
-	}
-	taskManagerInstance.AllTasks.Tasks["test1"].TaskMu.RUnlock()
-	taskManagerInstance.AllTasks.Tasks["test2"].TaskMu.RUnlock()
 }
 
 func singleQueueOverload(t *testing.T) {
+	t.Helper()
 	taskQueueOne, _ := assignTaskAndQueue(t, taskManagerInstance.AllTasks.Tasks["test1"])
 	taskQueueTwo, _ := assignTaskAndQueue(t, taskManagerInstance.AllTasks.Tasks["test2"])
 	totalRequests := 8
@@ -143,14 +37,7 @@ func singleQueueOverload(t *testing.T) {
 		testLoggingHelper(t, fmt.Sprintf("All requests not moved to Queue2. Total:: %d", totalRequests), true)
 	}
 
-	select {
-	case isReady := <-readyChan:
-		if !isReady {
-			t.Error("Queue 1 did not become ready after overload")
-		}
-	case <-time.After(10 * time.Second):
-		t.Error("Timeout waiting for queue 1 to become ready")
-	}
+	overloadChecker(t, nil, readyChan, "Queue Test1")
 
 	_, firstQueuedProcesses := assignTaskAndQueue(t, taskQueueOne)
 	_, secondQueuedProcesses = assignTaskAndQueue(t, taskQueueTwo)
@@ -164,4 +51,69 @@ func singleQueueOverload(t *testing.T) {
 	}
 	resetTaskQueue(t, taskQueueOne)
 	resetTaskQueue(t, taskQueueTwo)
+}
+
+func allQueueOverload(t *testing.T) {
+	t.Helper()
+	taskQueueOne, _ := assignTaskAndQueue(t, taskManagerInstance.AllTasks.Tasks["test1"])
+	taskQueueTwo, _ := assignTaskAndQueue(t, taskManagerInstance.AllTasks.Tasks["test2"])
+
+	totalRequests := 8
+
+	mockAddingRequests(t, (totalRequests / 2), taskQueueOne)
+	mockAddingRequests(t, (totalRequests / 2), taskQueueTwo)
+
+	readyChanOne := make(chan bool)
+	readyChanTwo := make(chan bool)
+
+	pendingChanOne := make(chan bool)
+	pendingChanTwo := make(chan bool)
+
+	taskQueueOne.TaskMu.Lock()
+	queueOne := taskQueueOne.QueuedProcesses[0]
+	taskQueueOne.QueuedProcesses = taskQueueOne.QueuedProcesses[1:]
+	taskQueueOne.TaskMu.Unlock()
+
+	taskQueueOne.UpdateOverloaded(queueOne, readyChanOne, pendingChanOne)
+
+	taskQueueTwo.TaskMu.Lock()
+	queueTwo := taskQueueTwo.QueuedProcesses[0]
+	taskQueueTwo.QueuedProcesses = taskQueueTwo.QueuedProcesses[1:]
+	taskQueueTwo.TaskMu.Unlock()
+
+	taskQueueTwo.UpdateOverloaded(queueTwo, readyChanTwo, pendingChanTwo)
+
+	taskManagerInstance.PendingTasks.PendingMu.RLock()
+	pendingQueue := len(taskManagerInstance.PendingTasks.PendingQueue)
+	if pendingQueue != totalRequests {
+		testLoggingHelper(t, fmt.Sprintf("Pending tasks not adding upto (%d). Pending:: %d", totalRequests, pendingQueue), true)
+	}
+	taskManagerInstance.PendingTasks.PendingMu.RUnlock()
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	go overloadChecker(t, &wg, readyChanOne, "Queue Test1")
+	go overloadChecker(t, &wg, readyChanTwo, "Queue Test2")
+	go overloadChecker(t, &wg, pendingChanOne, "Pending Test1")
+	go overloadChecker(t, &wg, pendingChanTwo, "Pending Test2")
+
+	if waitTimeout(&wg, 15*time.Second) {
+		t.Error("Timeout waiting for all channels to receive signals")
+	}
+
+	_, firstQueuedProcesses := assignTaskAndQueue(t, taskQueueOne)
+	_, secondQueuedProcesses := assignTaskAndQueue(t, taskQueueTwo)
+
+	if (firstQueuedProcesses + secondQueuedProcesses) != totalRequests {
+		testLoggingHelper(t, fmt.Sprintf("Tasks not adding upto total (%d)", totalRequests), true)
+	}
+
+	if firstQueuedProcesses == 0 {
+		testLoggingHelper(t, "Test1 queue empty after overload", true)
+	}
+	if secondQueuedProcesses == 0 {
+		testLoggingHelper(t, "Test2 queue empty after overload", true)
+	}
+
 }
